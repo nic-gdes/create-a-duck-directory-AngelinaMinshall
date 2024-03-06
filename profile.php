911c0901e1cf319d08560e629c456addac87e201
Angelina Minshall <atminshall@nic.edu>
2024-03-06 15:49:25 -0800

    php

--- profile.php
+++ profile.php
@@ -1,3 +1,28 @@
+<?php 
+
+$duck_is_live = false;
+
+if(isset($_GET['id'])) {
+    $id = htmlspecialchars($_GET['id']);
+
+    require('./config/db.php');
+
+    $sql = "SELECT id, name, favorite_foods, bio, img_src FROM ducks WHERE id =$id";
+    $result = mysqli_query($conn, $sql);
+
+    $duck = mysqli_fetch_assoc($result);
+
+    mysqli_free_result($result);
+    mysqli_close($conn);
+
+
+    if(isset($duck["id"])) {
+        $duck_is_live = true;
+    }
+
+    $duck_is_live = true;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,19 +36,38 @@ include ('./components/head.php');
 include ('./components/nav.php');
 ?>
     <section>
+
+        <?php if ($duck_is_live) : ?>
         <div class="profile-container">
-            <img class="duck-image" src="./assets/duck_profile.jpg" alt="Duck Profile Picture">
-            <h2>Name: Quackington</h2>
-            <p>Favorite Foods:</p>
+            <img class="" src="<?php echo $duck['img_src']; ?>" alt="duck.">
+
+            <h2> <?php echo $duck['name'];?> </h2>
+
+            <p> <?php echo $duck['bio']; ?></p>
+       
             <ul>
-                <li>Bread</li>
-                <li>Seeds</li>
-                <li>Insects</li>
+            <?php 
+                $foods_list = explode(",", $duck["favorite_foods"]);
+                // print_r($foods_list);
+            ?>
             </ul>
+            <ul class="favorite-foods">
+         <?php foreach($foods_list as $food) : ?>
+            <li> <?php echo $food ?></li>
+            <?php endforeach ?>
+            </ul>
+
             <p>Bio: Quackington is a friendly and curious duck who loves exploring ponds and interacting with other feathered friends. With a diet consisting of bread, seeds, and insects, Quackington leads a happy and quacktastic life!</p>
         </div>
     </section>
 
+    <?php else : ?>
+
+        <Section class="no-duck">
+            <h1>Sorry, no duck.</h1>
+        </Section>
+
+    <?php endif ?>
 <?php
 include ('./components/footer.php');
 ?>
